51e3e177f36e2f90ed21f9721b6d3095e76ce9a7
Cerebrovinny <vinny.cardoso96@gmail.com>
2021-07-31 17:45:37 -0300

    gulp structure spa build

---
+++ spa/gulpTasks/servidor.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+
+function filesMonitor(cb) {
+
+    return cb()
+}
+
+function server(cb) {
+    return cb()
+}
+
+module.exports = {
+    filesMonitor,
+    server
+}
