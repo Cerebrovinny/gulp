936ed2ad18743dca4846cda2f92857b7e577d826
Cerebrovinny <vinny.cardoso96@gmail.com>
2021-08-01 10:22:18 -0300

    gulp added server and files monitor

--- spa/gulpTasks/servidor.js
+++ spa/gulpTasks/servidor.js
@@ -1,15 +1,25 @@
 const gulp = require('gulp')
-
-function filesMonitor(cb) {
-
-    return cb()
-}
+const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
 
 function server(cb) {
+    return gulp.src('build')
+        .pipe(webserver({
+            port: 8080,
+            open: true,
+            livereload: true,
+        }))
+}
+
+function filesMonitor(cb) {
+    watch('src/**/*.html', () => gulp.series('appHTML')())
+    watch('src/**/*.scss', () => gulp.series('appCSS')())
+    watch('src/**/*.js', () => gulp.series('appJS')())
+    watch('src/assets/imgs/**/*.*', () => gulp.series('appIMG')())
     return cb()
 }
 
 module.exports = {
-    filesMonitor,
-    server
+    server,
+    filesMonitor
 }
